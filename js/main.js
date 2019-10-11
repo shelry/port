6552724f7a9ae2f837dfa27ce582f3823cebd11e
shelry <43773500+shelry@users.noreply.github.com>
2019-10-12 02:57:35 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -2,24 +2,8 @@ $(function(){
     //**초기값
     var num = 0
     $('.change_word li').eq(num).addClass('on')
-
-/*
-    변수 : 데이터 값이 변하는 경우 사용
-    - 선언 후 사용
-    - 변수 선언 : var 변수명 = 값
-    - 변수명 첫글자는 꼭! 영어로 시작!
-*/
-    //**1초마다 계속 실행되는 동작 설정
     var slide = function(){
-    //num = num +1
-    //num =+ 1
- /*   if문 : 조건문, 조건이 참일경우 실행
-    if(조건식){조건이 참일경우 실행}
-    if(조건식){조건이 참일경우 실행}else{조건이 참이아닐경우 실행}
-    if(조건식){조건이 참일경우 실행}else if{그렇지 않을 경우 조건이 참이면 실행}else{두 조건 다 참이 아닐경우 실행}
-    
-    - 조건식 : >, < >=, <=, ==(같다), !=(다르다)
-    */
+
         num = num +1
         if(num > 5){
             num = 0
@@ -27,9 +11,22 @@ $(function(){
         
     $('.change_word li').eq(num).addClass('on')
     $('.change_word li').eq(num).siblings().removeClass('on')
-    //$('li').removeClass('on')
-    //$('li').eq(num).addClass('on')
     }
-    
     setInterval(slide,1500)
+    function hoverIn() {
+        $(this).stop().animate({
+            'background-size':'120%'
+        },500)
+    }
+    function hoverOut() {
+        $(this).stop().animate({
+            'background-size':'100%'
+        },300)
+    }
+    $('#sec2 .project_list li').hover(hoverIn, hoverOut);
+
+      AOS.init({
+        easing: 'ease-in-out-sine'
+      });
+
 })//ready
