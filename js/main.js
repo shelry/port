211241d7b5b79e551cdb7c30f291825509c1f970
shelry <43773500+shelry@users.noreply.github.com>
2019-10-28 21:48:04 +0900

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(function(){
     //**초기값
     var num = 0
-    $('.change_word li').eq(num).addClass('on')
+    $('.change_word div').eq(num).addClass('on')
     var slide = function(){
 
         num = num +1
@@ -9,8 +9,8 @@ $(function(){
             num = 0
         }
         
-    $('.change_word li').eq(num).addClass('on')
-    $('.change_word li').eq(num).siblings().removeClass('on')
+    $('.change_word div').eq(num).addClass('on')
+    $('.change_word div').eq(num).siblings().removeClass('on')
     }
     setInterval(slide,1500)
     function hoverIn() {
@@ -24,4 +24,30 @@ $(function(){
         },300)
     }
     $('#sec2 .project_list li').hover(hoverIn, hoverOut);
+
+    
+    
+    // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+    
 })//ready
